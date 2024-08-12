00cd8f5b6f26e04b0cf6b90c185e360fd3fbb05d
Zepelown <dbswjdqhd12@gmail.com>
2024-08-13 02:16:39 +0900

    refactor: separate method for createing car object
    - Create CarRacingManager to separate method

--- src/main/kotlin/racingcar/controller/CarRacingGameController.kt
+++ src/main/kotlin/racingcar/controller/CarRacingGameController.kt
@@ -2,17 +2,19 @@ package racingcar.controller
 
 import racingcar.model.Car
 import racingcar.model.CarRacingGame
+import racingcar.service.CarRacingGameManager
 import racingcar.view.ConsoleInputView
 import racingcar.view.ConsoleOutputView
 
 class CarRacingGameController {
     private val consoleInputView : ConsoleInputView = ConsoleInputView()
     private val consoleOutputView : ConsoleOutputView = ConsoleOutputView()
+    private val carRacingGameManager : CarRacingGameManager = CarRacingGameManager()
 
     fun startGame(){
         consoleOutputView.printCarNameInputMessage()
         val carNames = consoleInputView.getCarNames()
-        val cars = createCars(carNames)
+        val cars = carRacingGameManager.createCars(carNames)
 
         consoleOutputView.printCarRacingRoundCountMessage()
         val gameRoundCount = consoleInputView.getCarRacingRoundCount()
@@ -23,12 +25,4 @@ class CarRacingGameController {
         consoleOutputView.printCarRacingGameWinners(carRacingGame.getWinners() ?: emptyList())
     }
 
-    private fun createCars(carNames : List<String>) : List<Car>{
-        val cars : MutableList<Car> = mutableListOf()
-        carNames.forEach {carName ->
-            val car = Car(carName)
-            cars.add(car)
-        }
-        return cars
-    }
 }
