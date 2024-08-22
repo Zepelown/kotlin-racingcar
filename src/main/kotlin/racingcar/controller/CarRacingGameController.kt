941e024a5f76b814cd22fe31cd2bc1139774e4af
Zepelown <dbswjdqhd12@gmail.com>
2024-08-23 00:54:51 +0900

    impove: add error handling for name input form to prompt re-entry on validation failure

--- src/main/kotlin/racingcar/controller/CarRacingGameController.kt
+++ src/main/kotlin/racingcar/controller/CarRacingGameController.kt
@@ -1,18 +1,19 @@
 package racingcar.controller
 
+import racingcar.model.Car
 import racingcar.model.CarRacingGameFactory
+import racingcar.view.ConsoleErrorView
 import racingcar.view.ConsoleInputView
 import racingcar.view.ConsoleOutputView
 
 class CarRacingGameController {
     private val consoleInputView : ConsoleInputView = ConsoleInputView()
     private val consoleOutputView : ConsoleOutputView = ConsoleOutputView()
-    private val carRacingGameFactory : CarRacingGameFactory = CarRacingGameFactory()
+    private val consoleErrorView : ConsoleErrorView = ConsoleErrorView()
+    private val carRacingGameFactory: CarRacingGameFactory = CarRacingGameFactory()
 
     fun startGame(){
-        consoleOutputView.printCarNameInputMessage()
-        val carNames = consoleInputView.getCarNames()
-        val cars = carRacingGameFactory.createCars(carNames)
+        val cars = createCarsFromInput()
 
         consoleOutputView.printCarRacingRoundCountMessage()
         val gameMaxRound = carRacingGameFactory.createGameMaxRound(consoleInputView.getCarRacingRoundCount())
@@ -21,6 +22,20 @@ class CarRacingGameController {
         val gameResults = carRacingGame.calculateRoundResult()
         consoleOutputView.printCarRacingGameResult(gameResults)
         consoleOutputView.printCarRacingGameWinners(carRacingGame.getWinners() ?: emptyList())
+    }
+
+    private fun createCarsFromInput() : List<Car>{
+        var cars : List<Car> = mutableListOf()
+        try {
+            consoleOutputView.printCarNameInputMessage()
+            val carNames = consoleInputView.getCarNames()
+            cars = carRacingGameFactory.createCars(carNames)
+
+        } catch (e : IllegalArgumentException){
+            consoleErrorView.printCarNamesInputErrorMsg()
+            createCarsFromInput()
+        }
+        return cars
     }
 
 }
---
+++ src/main/kotlin/racingcar/view/ConsoleErrorView.kt
@@ -0,0 +1,7 @@
+package racingcar.view
+
+class ConsoleErrorView {
+    fun printCarNamesInputErrorMsg(){
+        println("[ERROR] 자동차 이름 입력 양식이 올바르지 않습니다!! 예시) pobi,test")
+    }
+}
